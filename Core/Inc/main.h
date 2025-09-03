137aa7144e9af2fb49afffe06afef36f523339e6
pietros <pietro.levo.sc@gmail.com>
2025-09-03 11:55:22 +0200

    feat!: ioc and code re-generated
    Signed-off-by: pietros <pietro.levo.sc@gmail.com>

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,21 +57,16 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define BTN_TC_Pin GPIO_PIN_0
-#define BTN_TC_GPIO_Port GPIOA
-#define BTN_TC_EXTI_IRQn EXTI0_IRQn
-#define BTN_TV_Pin GPIO_PIN_1
-#define BTN_TV_GPIO_Port GPIOA
-#define BTN_TV_EXTI_IRQn EXTI1_IRQn
-#define BTN_LC_Pin GPIO_PIN_2
-#define BTN_LC_GPIO_Port GPIOA
-#define BTN_LC_EXTI_IRQn EXTI2_IRQn
-#define BTN_RTD_Pin GPIO_PIN_3
-#define BTN_RTD_GPIO_Port GPIOA
-#define BTN_RTD_EXTI_IRQn EXTI3_IRQn
-#define BTN_USER_Pin GPIO_PIN_4
-#define BTN_USER_GPIO_Port GPIOA
-#define BTN_USER_EXTI_IRQn EXTI4_IRQn
+#define BTN_1_Pin GPIO_PIN_0
+#define BTN_1_GPIO_Port GPIOA
+#define BTN_2_Pin GPIO_PIN_1
+#define BTN_2_GPIO_Port GPIOA
+#define BTN_3_Pin GPIO_PIN_2
+#define BTN_3_GPIO_Port GPIOA
+#define BTN_4_Pin GPIO_PIN_3
+#define BTN_4_GPIO_Port GPIOA
+#define BTN_5_Pin GPIO_PIN_4
+#define BTN_5_GPIO_Port GPIOA
 #define RSW1_C1_Pin GPIO_PIN_0
 #define RSW1_C1_GPIO_Port GPIOB
 #define RSW1_C2_Pin GPIO_PIN_1
@@ -86,8 +81,8 @@ void Error_Handler(void);
 #define RSW3_C4_GPIO_Port GPIOB
 #define RSW3_C8_Pin GPIO_PIN_15
 #define RSW3_C8_GPIO_Port GPIOB
-#define RSW1_C8_Pin GPIO_PIN_3
-#define RSW1_C8_GPIO_Port GPIOB
+#define RSW1_C8_Pin GPIO_PIN_8
+#define RSW1_C8_GPIO_Port GPIOA
 #define RSW2_C1_Pin GPIO_PIN_4
 #define RSW2_C1_GPIO_Port GPIOB
 #define RSW2_C2_Pin GPIO_PIN_5
--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -76,39 +76,29 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pins : BTN_TC_Pin BTN_TV_Pin BTN_LC_Pin BTN_RTD_Pin
-                           BTN_USER_Pin */
-  GPIO_InitStruct.Pin = BTN_TC_Pin|BTN_TV_Pin|BTN_LC_Pin|BTN_RTD_Pin
-                          |BTN_USER_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  /*Configure GPIO pins : BTN_1_Pin BTN_2_Pin BTN_3_Pin BTN_4_Pin
+                           BTN_5_Pin */
+  GPIO_InitStruct.Pin = BTN_1_Pin|BTN_2_Pin|BTN_3_Pin|BTN_4_Pin
+                          |BTN_5_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : RSW1_C1_Pin RSW1_C2_Pin RSW1_C4_Pin RSW3_C1_Pin
-                           RSW3_C2_Pin RSW3_C4_Pin RSW3_C8_Pin RSW1_C8_Pin
-                           RSW2_C1_Pin RSW2_C2_Pin RSW2_C4_Pin RSW2_C8_Pin */
+                           RSW3_C2_Pin RSW3_C4_Pin RSW3_C8_Pin RSW2_C1_Pin
+                           RSW2_C2_Pin RSW2_C4_Pin RSW2_C8_Pin */
   GPIO_InitStruct.Pin = RSW1_C1_Pin|RSW1_C2_Pin|RSW1_C4_Pin|RSW3_C1_Pin
-                          |RSW3_C2_Pin|RSW3_C4_Pin|RSW3_C8_Pin|RSW1_C8_Pin
-                          |RSW2_C1_Pin|RSW2_C2_Pin|RSW2_C4_Pin|RSW2_C8_Pin;
+                          |RSW3_C2_Pin|RSW3_C4_Pin|RSW3_C8_Pin|RSW2_C1_Pin
+                          |RSW2_C2_Pin|RSW2_C4_Pin|RSW2_C8_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+  /*Configure GPIO pin : RSW1_C8_Pin */
+  GPIO_InitStruct.Pin = RSW1_C8_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(RSW1_C8_GPIO_Port, &GPIO_InitStruct);
 
 }
 
